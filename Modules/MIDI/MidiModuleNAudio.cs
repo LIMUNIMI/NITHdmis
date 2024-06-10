f8bc655841e014b746da9fdc50b173f50b982efe
Neeqstock <nicola.davanzo01@gmail.com>
2024-06-10 17:28:04 +0200

    Update MidiModuleNAudio.cs

--- Modules/MIDI/MidiModuleNAudio.cs
+++ Modules/MIDI/MidiModuleNAudio.cs
@@ -1,156 +1,171 @@
 ﻿using NAudio.Midi;
-
-namespace NITHdmis.Modules.MIDI
+using System;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+namespace NITHdmis.MIDI
 {
     /// <summary>
     /// A MIDI controller module based on the library NAudio.
-    /// </summary>
-    public class MidiModuleNAudio : IMidiModule
-    {
-        private int midiChannel = 0;
-        private bool midiOk = false;
-        private MidiOut midiOut;
-        private int outDevice = 0;
-        public int MidiChannel { get => midiChannel; set { midiOk = false;  midiChannel = value; ResetMidiOut(); } }
-        public int OutDevice { get => outDevice; set { midiOk = false; outDevice = value; ResetMidiOut(); } }
-        
+    /// </summary>    
 
-        public MidiModuleNAudio(int outDevice, int midiChannel)
+        public class MidiModuleNAudio : IMidiModule
         {
-            this.outDevice = outDevice;
-            this.midiChannel = midiChannel;
+            private int midiChannel = 0;
+            private bool midiOk = false;
+            private MidiOut midiOut;
+            private int outDevice = 0;
 
-            ResetMidiOut();
-        }
+            public int MidiChannel { get => midiChannel; set { midiOk = false; midiChannel = value; ResetMidiOut(); } }
+            public int OutDevice { get => outDevice; set { midiOk = false; outDevice = value; ResetMidiOut(); } }
 
-        public bool IsMidiOk()
-        {
-            return (midiOk);
-        }
+            public MidiModuleNAudio(int outDevice, int midiChannel)
+            {
+                this.outDevice = outDevice;
+                this.midiChannel = midiChannel;
+                ResetMidiOut();
+            }
 
-        public void PlayNote(int pitch, int velocity)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.StartNote(pitch, velocity, MidiChannel).RawData);
-        }
+            public bool IsMidiOk()
+            {
+                return midiOk;
+            }
 
-        public void SendMessage(int byte1, int byte2, int byte3)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.ChangeControl(byte1, byte2, midiChannel).RawData);
-        }
+            public void PlayNote(int pitch, int velocity)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.StartNote(pitch, velocity, midiChannel).RawData);
+            }
 
-        public void SetVelocity(int velocity)
+            public void SendMessage(int byte1, int byte2, int byte3)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(byte1, byte2, midiChannel).RawData);
+            }
+
+            public void SetExpression(int expression)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(11, expression, midiChannel).RawData);
+            }
+
+            public void SetModulation(int modulation)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(1, modulation, midiChannel).RawData);
+            }
+
+            public void SetModulationRate(int modulationRate)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(19, modulationRate, midiChannel).RawData);
+            }
+
+            public void ToggleTremolo(bool activate, int baseNote, int range)
+            {
+                int tremoloValue = activate ? 127 : 0; // 127 represents full activation, 0 full deactivation
+                if (activate)
+                {
+                    int trillNote = baseNote + range;
+                    // Send both notes to the MIDI module to handle the trill effect
+                    SetTrillNotes(baseNote, trillNote);
+                }
+                SetTremolo(tremoloValue);
+            }
+
+            public void SetTremolo(int tremolo)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(13, tremolo, midiChannel).RawData);
+            }
+
+            public void SetPitchBend(int pitchBendValue)
+            {
+                // Set limits
+                if (pitchBendValue > 16383)
+                    pitchBendValue = 16383;
+                if (pitchBendValue < 0)
+                    pitchBendValue = 0;
+
+                int lsb = pitchBendValue & 0b1111111;
+                int msb = (pitchBendValue & 0b1111111_0000000) >> 7;
+                int status = 0b1110 << 4;
+
+                MidiMessage message = new MidiMessage(status, lsb, msb);
+
+                if (midiOut != null && midiOk)
+                    midiOut.Send(message.RawData);
+            }
+
+            public void SetPitchNoBend()
+            {
+                if (midiOut != null && midiOk)
+                    SetPitchBend(8192);
+            }
+
+        public void SetAttackRampSpeed(int attack)
         {
             if (midiOut != null && midiOk)
-            {
-                // Invia un messaggio "Note On" con la nuova velocity
-               midiOut.Send(MidiMessage.ChangeControl(0, velocity, midiChannel).RawData);
-            
-            }
-        }
-
-        public void SetExpression(int expression)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.ChangeControl(11, expression, midiChannel).RawData);
-        }
-
-        public void SetModulation(int modulation)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.ChangeControl(1, modulation, midiChannel).RawData);
-        }
-
-        public void SetPitchBend(int pitchBendValue)
-        {
-            // Set limits
-            if (pitchBendValue > 16383)
-                pitchBendValue = 16383;
-            if (pitchBendValue < 0)
-                pitchBendValue = 0;
-
-            int lsb = pitchBendValue & 0b1111111;
-            int msb = (pitchBendValue & 0b1111111_0000000) >> 7;
-            int status = 0b1110 << 4;
-
-            MidiMessage message = new MidiMessage(status, lsb, msb);
-
-            if(midiOut!=null && midiOk)
-                midiOut.Send(message.RawData);
-        }
-
-        public void SetPitchNoBend()
-        {
-            if(midiOut!=null && midiOk)
-                SetPitchBend(8192);
+                midiOut.Send(MidiMessage.ChangeControl(9, attack, midiChannel).RawData);
         }
 
         public void SetPressure(int pressure)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.ChangeControl(7, pressure, midiChannel).RawData);
-        }
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.ChangeControl(8, pressure, midiChannel).RawData);
+            }
 
-        public void StopNote(int pitch)
-        {
-            if(midiOut!=null && midiOk)
-                midiOut.Send(MidiMessage.StopNote(pitch, 0, MidiChannel).RawData);
-        }
-
-        private bool switchTrill = false;
-
-        public void SetTrill(int pitchTrillValue)
-        {
-            if (midiOut != null && midiOk)
-            if (switchTrill == false)
+            public void SetPanic()
+            {
+                if (midiOut != null && midiOk)
                 {
-                    pitchTrillValue+=4;
+                    midiOut.Send(MidiMessage.ChangeControl(127, 127, 15).RawData);
                 }
-            else if(switchTrill == true)
+            }
+
+            public void StopNote(int pitch)
+            {
+                if (midiOut != null && midiOk)
+                    midiOut.Send(MidiMessage.StopNote(pitch, 0, midiChannel).RawData);
+            }
+
+            private bool switchTrill = false;
+
+            private void SetTrillNotes(int baseNote, int trillNote)
+            {
+                if (midiOut != null && midiOk)
                 {
-                    pitchTrillValue-=4;
+                    if (switchTrill)
+                    {
+                        PlayNote(baseNote, 100); // Adjust velocity as needed
+                    }
+                    else
+                    {
+                        PlayNote(trillNote, 100); // Adjust velocity as needed
+                    }
+                    switchTrill = !switchTrill;
                 }
-
-            switchTrill = !switchTrill;
-
-            int lsb = pitchTrillValue & 0b1111111;
-            int msb = (pitchTrillValue & 0b1111111_0000000) >> 7;
-            int status = 0b1110 << 4;
-            MidiMessage message = new MidiMessage(status, lsb, msb);
-            midiOut.Send(message.RawData);
-        }
-
-        public void SetNoTrill()
-        {
-            if (midiOut != null && midiOk)
-                SetTrill(8192);
-        }
-
-        private void ResetMidiOut()
-        {
-            if(midiOut != null)
-            {
-                midiOut.Close();
-                midiOut.Dispose();
             }
-            try
-            {
 
-                midiOut = new MidiOut(this.OutDevice);
-                midiOk = true;
-            }
-            catch
+            private void ResetMidiOut()
             {
-                midiOk = false;
+                if (midiOut != null)
+                {
+                    midiOut.Close();
+                    midiOut.Dispose();
+                }
+                try
+                {
+                    midiOut = new MidiOut(this.OutDevice);
+                    midiOk = true;
+                }
+                catch
+                {
+                    midiOk = false;
+                }
+            }
+
+            public void PlayNote(object tag, int v)
+            {
+                throw new NotImplementedException();
             }
         }
-
-        public void PlayNote(object tag, int v)
-        {
-            throw new NotImplementedException();
-        }
-
-       
     }
-}
